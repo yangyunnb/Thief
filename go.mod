0992af4942333be9af4e4391cf1c50b04ae8c1ed
tmacyangyun <tmacyangyun@didiglobal.com>
2021-03-10 16:29:34 +0800

    添加golangCI配置文件、添加go.mod配置文件

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/Thief.git
+
+go 1.15
--- main.go
+++ main.go
@@ -3,7 +3,8 @@ package main
 import "fmt"
 
 func main() {
-	//我是dev分支的代码
-	//我是master分支的代码
-	fmt.Println("hello world")
+	// 我是dev分支的代码
+	// 我是master分支的代码
+	name := "yangyun"
+	fmt.Printf("hello world:%s\n", name)
 }
