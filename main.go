3d44d3c0e369d4e7bfc982b07eab902ea7d8cf7c
tmacyangyun <tmacyangyun@didiglobal.com>
2021-03-11 11:47:40 +0800

    增加server优雅关闭代码

--- main.go
+++ main.go
@@ -1,6 +1,12 @@
 package main
 
 import (
+	"context"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
+
 	"github.com/Thief.git/controller"
 	"github.com/kataras/iris"
 	"github.com/kataras/iris/mvc"
@@ -8,11 +14,29 @@ import (
 
 func main() {
 	app := iris.New()
+	doneChan := make(chan bool, 1)
 
-	routerPart := app.Party("/")
-	mvc.New(routerPart).Handle(&controller.Health{})
+	go func() {
+		signalChan := make(chan os.Signal, 1)
+		signal.Notify(signalChan, os.Interrupt, syscall.SIGINT, syscall.SIGQUIT, syscall.SIGKILL, syscall.SIGTERM)
 
-	if err := app.Run(iris.Addr(":8080")); err != nil {
+		select {
+		case <-signalChan:
+			ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+			defer cancel()
+
+			<-ctx.Done()
+			if err := app.Shutdown(ctx); err != nil {
+				app.Logger().Error(err.Error())
+			}
+			close(doneChan)
+		}
+	}()
+
+	healthPart := app.Party("/")
+	mvc.New(healthPart).Handle(&controller.Health{})
+	if err := app.Run(iris.Addr(":8080"), iris.WithoutInterruptHandler); err != nil {
 		panic(err)
 	}
+	<-doneChan
 }
